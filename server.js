548efcffa98f94ac1269cf4b8a6c98d1d3b3b64e
fahmistorecloud <iqeyhsuwardy27@gmail.com>
2026-07-11 06:03:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ========== DATABASE ==========
 const dbPath = path.join(__dirname, 'tradingpro.db');
